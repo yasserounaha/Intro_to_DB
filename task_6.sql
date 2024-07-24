e11f45e540d772e7c97b91e07ad7c407a0accf5f
yasserounaha <yasser12001@gmail.com>
2024-07-24 16:52:24 -0700

    Add SQL script to insert multiple rows into the customer table

--- task_6.sql
+++ task_6.sql
@@ -4,7 +4,7 @@ USE alx_book_store;
 -- Insert multiple rows into the customer table
 INSERT INTO customer (customer_id, customer_name, email, address) 
 VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
 
